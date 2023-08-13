8c7252fdd3c399e07396ec83d59ee2ed9e185b7c
Roy <92612927+PetarWho@users.noreply.github.com>
2023-08-13 09:16:41 +0300

    Groups

--- Quizy/accounts/groups.py
+++ Quizy/accounts/groups.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import Group, Permission
 
+from Quizy.accounts.models import AppUser
+
 superuser_group, _ = Group.objects.get_or_create(name='Superuser')
 staff_group, _ = Group.objects.get_or_create(name='Staff')
 
@@ -9,3 +11,16 @@ superuser_group.permissions.set(all_permissions)
 
 staff_permissions = all_permissions.filter(content_type__model__in=['category', 'question'], codename__in=['add', 'change'])
 staff_group.permissions.set(staff_permissions)
+
+user = AppUser
+moderator_group, created = Group.objects.get_or_create(name='Moderator')
+admin_group, created2 = Group.objects.get_or_create(name='Admin')
+
+user.is_staff = True
+user.save()
+user.groups.add(moderator_group)
+
+user.is_superuser = True
+user.is_staff = True
+user.save()
+user.groups.add(admin_group)
